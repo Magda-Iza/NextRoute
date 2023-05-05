c3d4193f641ab870eb34c830f38ffb6dbb213ad5
MagdalenaBrejna <magdalena.brejna@wp.pl>
2023-05-05 16:08:31 +0200

    Dodanie sso przy pomocy oauth2 i Octa

--- src/main/java/ztw/nextapp/NextRouteApplication.java
+++ src/main/java/ztw/nextapp/NextRouteApplication.java
@@ -2,11 +2,29 @@ package ztw.nextapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
 @RestController
 public class NextRouteApplication {
+
+	@GetMapping("/")
+	public String echoTheUsersEmailAddress(Principal principal) {
+		return "Hey there! Your email address is: ";
+	}
+
+	@GetMapping("/login")
+	public String log(Principal principal) {
+		return "Logowanie";
+	}
+
+	@GetMapping("/cos")
+	public String cos(Principal principal) {
+		return "C";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NextRouteApplication.class, args);

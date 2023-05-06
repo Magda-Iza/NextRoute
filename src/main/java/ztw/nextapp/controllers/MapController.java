316222ab4f1a6c5c4da25f96f73e5f9c17df9a33
IzabelaRosinska <91902002+IzabelaRosinska@users.noreply.github.com>
2023-05-06 11:32:55 +0200

    Add directions api

--- src/main/java/ztw/nextapp/controllers/MapController.java
+++ src/main/java/ztw/nextapp/controllers/MapController.java
@@ -1,5 +1,11 @@
 package ztw.nextapp.controllers;
 
+import com.google.maps.DirectionsApi;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
+import com.google.maps.model.TravelMode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +14,8 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 import ztw.nextapp.web.model.geocoding.Response;
+
+import java.util.ArrayList;
 
 @RestController
 public class MapController {
@@ -40,21 +48,68 @@ public class MapController {
         return response.getBody();
     }
 
-    @GetMapping("/getDirections")
-    public Response getDirections(@RequestParam String origin, @RequestParam String destination) {
-        UriComponents uri = UriComponentsBuilder.newInstance()
-                .scheme("https")
-                .host("maps.googleapis.com")
-                .path("/maps/api/directions/v2:computeRoutes/json")
-                .queryParam("key", API_KEY)
-                .queryParam("origin", origin)
-                .queryParam("destination", destination)
-                .queryParam("travelMode", "DRIVE")
-                .build();
+//    @GetMapping("/getDirections")
+//    public Response getDirections(@RequestParam String origin, @RequestParam String destination) {
+//        UriComponents uri = UriComponentsBuilder.newInstance()
+//                .scheme("https")
+//                .host("maps.googleapis.com")
+//                .path("/maps/api/directions/v2:computeRoutes/json")
+//                .queryParam("key", API_KEY)
+//                .queryParam("origin", origin)
+//                .queryParam("destination", destination)
+//                .queryParam("travelMode", "DRIVE")
+//                .build();
+//
+//        ResponseEntity<Response> response = new RestTemplate().getForEntity(uri.toUriString(), Response.class);
+//        System.out.println(response.getBody());
+//        return response.getBody();
+//    }
 
-        ResponseEntity<Response> response = new RestTemplate().getForEntity(uri.toUriString(), Response.class);
-        System.out.println(response.getBody());
-        return response.getBody();
+    // use java client for google maps directions api
+//    @GetMapping("/getDirections")
+//    public DirectionsApi.Response getDirections() {
+////        @RequestParam String origin, @RequestParam ArrayList<String> waypoints
+//        String origin = "kolkata";
+//        String destination = "delhi";
+//        ArrayList<String> waypoints = new ArrayList<>();
+//        waypoints.add("mumbai");
+//        DirectionsApiRequest directionsApiRequest = DirectionsApi.newRequest(null);
+//        directionsApiRequest.origin(origin);
+////        directionsApiRequest.destination(destination);
+//        directionsApiRequest.mode(TravelMode.valueOf("DRIVING"));
+//        directionsApiRequest.optimizeWaypoints(true);
+//        directionsApiRequest.waypoints(waypoints.toArray(new String[waypoints.size()]));
+//
+//        ResponseEntity<DirectionsApi.Response> response = new RestTemplate().getForEntity(directionsApiRequest.toString(), DirectionsApi.Response.class);
+//        return response.getBody();
+//    }
+
+    @GetMapping("/getDirections")
+    public DirectionsRoute getDirections() {
+        String origin = "kolkata";
+        String destination = "delhi";
+        ArrayList<String> waypoints = new ArrayList<>();
+        waypoints.add("mumbai");
+
+        // Create a new instance of the Directions API client
+        GeoApiContext context = new GeoApiContext.Builder()
+                .apiKey("AIzaSyAHy9ZHFybi2s9KD46oJqQ0_ZVhoSmsexQ")
+                .build();
+        DirectionsApiRequest directionsApiRequest = DirectionsApi.newRequest(context);
+        directionsApiRequest.origin(origin);
+        directionsApiRequest.destination(destination);
+        directionsApiRequest.mode(TravelMode.DRIVING);
+        directionsApiRequest.optimizeWaypoints(true);
+        directionsApiRequest.waypoints(waypoints.toArray(new String[waypoints.size()]));
+
+        try {
+            // Send the API request and get the response
+            DirectionsResult result = directionsApiRequest.await();
+            return result.routes[0];
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 //    @GetMapping("/getPlacePicker")
